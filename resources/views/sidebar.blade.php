3f0839785c4f764230dc102b066fc5d706accfdb
chrisye909 <yjx909@sina.com>
2016-12-01 15:17:03 +0800

    fontawesome 图标代替图片

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -3,19 +3,19 @@
         <img src="{!! asset('img/avatar.jpg') !!}" alt="avatar">
         <div class="caption">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-4" style="padding: 0px 47px 0px 47px;">
                     <a target="_blank" href="http://weibo.com/yjx306866027">
-                        <img src="{!! asset('img/sina.png') !!}" style="height:25px;width:30px">
+                        <i class="fa fa-weibo fa-2x" aria-hidden="true"></i>
                     </a>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4"  style="padding: 0px 47px 0px 47px;">
                     <a target="_blank" href="https://github.com/chrisye909">
-                    <img src="{!! asset('img/github.png') !!}" style="height:25px;width:25px">
+                        <i class="fa fa-github fa-2x" aria-hidden="true"></i>
                     </a>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4"  style="padding: 0px 47px 0px 47px;">
                     <a href="mailto:yjx909@sina.com">
-                    <img src="{!! asset('img/mail.png') !!}" style="height:25px;width:25px">
+                    <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
                     </a>
                 </div>
             </div>
