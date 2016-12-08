f614efd810931eb3ed378cc68301701962fddabd
chrisye909 <yjx909@sina.com>
2016-12-08 10:56:11 +0800

    修改markdown service provider 配置

--- config/app.php
+++ config/app.php
@@ -233,6 +233,6 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Flash' => Laracasts\Flash\Flash::class,
-        'Markdown' => 'GrahamCampbell\Markdown\Facades\Markdown'
+        'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class
     ],
 ];
